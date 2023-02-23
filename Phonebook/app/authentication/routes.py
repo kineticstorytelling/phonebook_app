9aff2a22257df4b22d3d7b555ce20882f7f3a261
kineticstorytelling <kineticstorytelling@gmail.com>
2023-02-22 20:11:41 -0500

    added to the authentication form

---
+++ Phonebook/app/authentication/routes.py
@@ -0,0 +1,28 @@
+from forms import UserLoginForm
+from models import User, db, check_password_hash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
+# imports for flask login 
+from flask_login import login_user, logout_user, LoginManager, current_user, login_required
+
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = UserLoginForm
+
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email, password)
+
+            user = User(email, password = password)
+
+            db.session.add(user)
+            db.session.commit()
+
+
+
+            flash(f'You have successfully created a user account {email}', 'User-created')
+            return redirect(url_for('site.home'))
